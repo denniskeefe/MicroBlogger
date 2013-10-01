be328acb477234653f978e801a0ef4c0c6370422
Dennis Keefe <dennis@thecommongeek.com>
2013-09-30 21:20:29 -0500

    step 3

--- lib/micro_blogger.rb
+++ lib/micro_blogger.rb
@@ -1,18 +1,35 @@
 require 'jumpstart_auth'
 
+
+
 class MicroBlogger
   attr_reader :client
 
   def dm(target, message)
      puts "d #{target} Trying to send this DM:"
      @client.direct_message_create(target, message)
+     screen_names = @client.followers.collect{|follower| follower.screen_name}
   end
+
+  
 
   def tweet(message)
     if message.length <= 140
       @client.update(message)
     else
       puts "Sorry, your tweet length was greater than 140 characters. Please compose a shorter message and try again."
+    end
+  end
+
+  def spam_my_followers(message)
+    get_followers.each do |follower|
+      dm(follower, message)
+    end
+  end
+
+  def get_followers
+    screen_names = @client.followers.collect do |follower|
+      follower.screen_name
     end
   end
 
@@ -28,6 +45,7 @@ class MicroBlogger
       when 'q' then puts "Goodbye!"
       when 't' then tweet(parts[1..-1].join(" "))
       when 'dm' then dm(parts[1], parts[2..-1].join(" "))
+      when 'spam' then spam_my_followers parts [1..-1].join(" ")
     else
       puts "Sorry, I don't know how to (#{command})"
   end
