ae72a7675a659d83f38c2f667fedc6166589993a
Dennis Keefe <dennis@thecommongeek.com>
2013-10-01 22:43:09 -0500

    last tweet

--- lib/micro_blogger.rb
+++ lib/micro_blogger.rb
@@ -33,24 +33,42 @@ class MicroBlogger
     end
   end
 
+  def everyones_last_tweet
+    friends = @client.friends
+    friends.sort_by! { |friend| friend.screen_name.downcase }
+    friends.each do |friend|
+      last_tweet = friend.status.text
+      timestamp  = friend.status.created_at
+      print "#{friend.screen_name} just said... "
+      print last_tweet
+      print " published #{timestamp.strftime('%A, %b %d')}"
+      puts "" #Just print a blank line to separate people
+    end
+  end
+
   def run
-    command = ""
-    while command != "q"
+    input = ""
+    while input != "q"
       puts ""
     printf "enter command: "
     input = gets.chomp
     parts = input.split
     command = parts[0]
+    process_command(command, parts)
+  end
+end
+def process_command(command, parts)
     case command
       when 'q' then puts "Goodbye!"
       when 't' then tweet(parts[1..-1].join(" "))
       when 'dm' then dm(parts[1], parts[2..-1].join(" "))
       when 'spam' then spam_my_followers parts [1..-1].join(" ")
+      when "elt"   then everyones_last_tweet
     else
       puts "Sorry, I don't know how to (#{command})"
   end
   end
-  end
+  
 
   def initialize
     puts "Initializing..."
